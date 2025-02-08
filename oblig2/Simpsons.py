18e527805593f3105e9cccf763fe46c29cfd009a
borgebj <borgetull@gmail.com>
2025-02-08 03:48:12 +0100

    Oblig 2 progress, ferdig m/ (1)

--- oblig2/Simpsons.py
+++ oblig2/Simpsons.py
@@ -2,18 +2,30 @@ import sys
 from rdflib import Graph, Namespace
 from rdflib.namespace import RDF, RDFS, FOAF
 
-
-g = Graph()
-
-g.print()
+# RDF graph defined
+graph = Graph()
 
 
 def main():
+
     # Command line arguments
-    if len(sys.argv) > 1:
-        rdf_path = sys.argv[0]
-        out_name = sys.argv[1]
-        print(f"Path: {sys.argv[0]}\nOut: {sys.argv[1]}")
+    if len(sys.argv) > 2:
+        in_path = sys.argv[1]
+        out_name = sys.argv[2]
+
+        # 1. Read input from file, add to graph
+        graph.parse(in_path, format="turtle")
+
+        # read through namespaces, add to graph
+        for name, uri in graph.namespaces():
+
+            prefix = Namespace(uri)
+            graph.bind(name, prefix)
+
+            print(f"@Prefix {name}: <{uri}>")
+
+    else:
+        print(f"Missing arguments <input_file> <output_file>")
 
 
 if __name__ == "__main__":
